34436084cefd07ba04b55c93d94d5349274625f7
Clément Targe <clementtarge@gmail.com>
2022-06-03 14:02:09 +0200

    azerty

--- colorize.py
+++ colorize.py
@@ -10,6 +10,10 @@ from  torchvision.utils import make_grid #to generate image grids, will be used 
 from data_utils import get_colorized_dataset_loader # dataloarder
 from unet import UNet
 
+import tensorflow as tf
+import tensorboard as tb
+tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+
 # setting device on GPU if available, else CPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -59,16 +63,30 @@ if __name__=='__main__':
     epochs = args.epochs
     lr = args.lr
     unet = UNet().to(device)
-    loader = get_colorized_dataset_loader(path=data_path, 
-                                        batch_size=batch_size, 
-                                        shuffle=True, 
-                                        num_workers=0)
-
+    loader = get_colorized_dataset_loader(path=data_path,
+                                          batch_size=batch_size,
+                                          shuffle=True,
+                                          num_workers=0)
 
     optimizer = optim.Adam(unet.parameters(), lr=lr)
     writer = SummaryWriter(f'runs/{exp_name}')
     train(unet, optimizer, loader, epochs=epochs, writer=writer)
-    writer.add_graph(unet)
+    x, y = next(iter(loader))
+
+    with torch.no_grad():
+        all_embeddings = []
+        all_labels = []
+        for x, y in loader:
+            x, y = x.to(device), y.to(device)
+            embeddings = unet.get_features(x).view(-1, 128 * 28 * 28)
+            all_embeddings.append(embeddings)
+            all_labels.append(y)
+            if len(all_embeddings) > 6:
+                break
+        embeddings = torch.cat(all_embeddings)
+        labels = torch.cat(all_labels)
+        writer.add_embedding(embeddings, label_img=labels, global_step=1)
+        writer.add_graph(unet, x.to(device))
 
     # Save model weights
     torch.save(unet.state_dict(), 'unet.pth')
